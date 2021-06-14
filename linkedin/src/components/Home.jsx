a871eb92d733315776c7b850bee596c7dca38bd8
jasko255 <jasko255@gmail.com>
2021-06-14 21:19:31 +0200

    navbar and profile

---
+++ linkedin/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap'
+// import NavBar from './NavBar'
+import ProfileSection from './ProfileSection'
+
+const Home = () => {
+    return ( 
+        <>
+        <Container>
+        <ProfileSection />
+        </Container>
+        </>
+     );
+}
+ 
+export default Home;
