da612bfe2a455645dc83e46b2547a9ecd384e174
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-14 18:16:54 +0100

    Update Experience.jsx

---
+++ linkedin/src/components/Experience.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+import { Col, Row } from 'react-bootstrap'
+
+class Experience extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <Row>
+                <Col md={8}>
+                    
+                </Col>
+
+            </Row>
+        );
+    }
+}
+ 
+export default Experience;
