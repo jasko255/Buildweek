311f93ead0bbbd8cbb8c8db828aeac7ff37e2617
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-06-15 01:03:18 +0100

    Update people-also-viewed.jsx

--- linkedin/src/components/people-also-viewed.jsx
+++ linkedin/src/components/people-also-viewed.jsx
@@ -5,7 +5,7 @@ import { Card, Col, Button, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 import DefaultProfile from "../assets/default_profile.jpeg"
 
@@ -34,26 +34,32 @@ class AlsoView extends Component {
 
 
     render() {
+
+        if seemore true map 12 if see more false map 6
       return (
-        <Card fluid="true" style={{ width: '100%' }} className="d-flex">
-            <Card.Header>People also viewed</Card.Header>
+        <Card fluid="true" style={{ width: '100%' }} className="d-flex px-0">
+            <Card.Header className="bg-white">People also viewed</Card.Header>
             <Card.Body>
                 <Card fluid="true" style={{ width: '100%' }} className="d-flex border border-white">
                     <Row>
-                        <Col fluid="true" md={4} lg={4} className="d-inline-flex">
+                        <Col fluid="true" md={4} lg={4} className="d-inline-flex ml-1 px-4 align-items-center">
                             <Card.Img src={DefaultProfile} className="rounded-circle"></Card.Img>
                         </Col>
-                        <Col fluid="true" md={8} lg={8} className="d-inline-flex">
-                            <Card.Title>James Shaw</Card.Title>
-                            <Card.Text>Architect at Darling Associates Architects</Card.Text>
+                        <Col fluid="true" md={8} lg={8} className="flex-column px-0 align-items-center">
+                            <Card.Title className="d-inline-flex container-fluid px-0">James Shaw</Card.Title>
+                            <Card.Text className="d-inline-flex container-fluid px-0">Architect at Darling Associates Architects</Card.Text>
                         </Col>
                     </Row>
-                    <Card.Subtitle className="d-inline-flex container-fluid">
-                    <Button className="d-flex mx-auto rounded-pill btn-primary bg-white text-primary border-2 px-4 justify-self-center">Connect</Button>
+                    <Card.Subtitle className="d-inline-flex container-fluid offset-3">
+                        <Button className="d-flex rounded-pill btn-primary bg-white text-primary border-2 px-4">Connect</Button>
                     </Card.Subtitle>
                 </Card>
             </Card.Body>
-            <Card.Footer>Show more</Card.Footer>
+
+            <Card.Footer className="text-center">
+                    <p className="d-inline-flex" onClick="handleMore()">Show more</p>
+                    <FontAwesomeIcon icon={faCaretDown} className="d-inline-flex"/>
+            </Card.Footer>
         </Card>
 
       )}
